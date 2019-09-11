a42043aad7934e092649c19f1eacc0b9bb123b9c
Loren <lriedy@uchicago.edu>
2019-09-11 15:04:02 -0400

    Updated Code

--- lottery.R
+++ lottery.R
@@ -11,14 +11,18 @@ library(stats)
 
 # Small edit! from class 1!
 
-arr <- array(0,10)
-for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
+num_of_coin_flips <- 10
+
+#Generate one lottery ticket
+lottery_ticket <- array(0,num_of_coin_flips)
+for (digit_num_in_ticket in 1:num_of_coin_flips){
+  lottery_ticket[digit_num_in_ticket] <- if (runif(1) < 0.1) 1 else 0
 }
 
+#Generate 100 lottery tickets
 for (j in 1:100){
-  arr <- array(0,10)
-  for (i in 1:10){
+  arr <- array(0,num_of_coin_flips)
+  for (i in 1:num_of_coin_flips){
     arr[i] <- if (runif(1) < 0.1) 1 else 0
   }
   print(arr)
